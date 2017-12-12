5faed9cbb76ad4eb67404b0c9f9b1a2f088ad664
yqwoe <yqwoe@live.cn>
2017-12-12 15:40:20 +0800

    基础联系

---
+++ hello.go
@@ -0,0 +1,9 @@
+package	main	
+import "fmt"
+var age = 12;
+
+func main(){
+
+	fmt.Println("Hello, World!!!!",age)
+	fmt.Println("Hello, World!!!!1111")
+}
---
+++ identifier.go
@@ -0,0 +1,25 @@
+package main
+
+
+var a = "菜鸟教程"
+var b string ="runoob.com"
+var c bool
+
+
+var (
+	e int
+	f string
+	g bool
+)
+
+var h,i string= "a","b"
+
+
+
+func main(){
+	j,k := 123,"abc"
+	println(a,b,c)
+	println(e,f,g)
+	println(h,i)
+	println(j,k)
+}
